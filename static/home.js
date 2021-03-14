4669fead21e7923130934890b95f5bac5cf2d848
Kevin Samson <kevinsam2003@gmail.com>
2021-03-14 18:03:05 +0400

    Part2:

--- static/home.js
+++ static/home.js
@@ -9,3 +9,14 @@ function Reset(){
     Add1()
 }
 
+const passwordInput = document.getElementById('password-input');
+const toggleButton = document.getElementById('toggle-password');
+
+toggleButton.addEventListener('click',() => {
+    console.log(passwordInput.type)
+    if (passwordInput.type === 'password'){
+        passwordInput.type = 'text';
+    }else if (passwordInput.type === 'text'){
+        passwordInput.type = 'password';
+    }
+});
